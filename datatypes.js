9df7bd8fd5cba1925e34a5dd1078b9dea9b82669
prasannadevapujyula <sailakshmiprasanna0707@gmail.com>
2024-08-25 22:31:01 +0530

    commit

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+//string
+let user= ("this is a string datatype")
+console.log(user)
+// number
+console.log(23456)
+//boolean
+y=500
+x=550
+console.log(x<y)
+//null
+let z = null
+console.log(z)
+//undefined
+let apple;
+console.log(apple)
---
+++ variables.js
@@ -0,0 +1,10 @@
+
+// let is once defined cannot be redefined but the value can be changed again
+let username = "prasanna"
+console.log(username)
+// once declared, var can be redefined and redeclared
+var num = 23
+console.log(num)
+//once declared , const cannot be redefined and redeclared
+const age = 22
+console.log(age)
