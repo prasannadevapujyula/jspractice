547627297d89e208bf08b7e9b9d765093a73cdb1
prasannadevapujyula <sailakshmiprasanna0707@gmail.com>
2024-08-26 21:15:45 +0530

    commit

---
+++ ifelse.js
@@ -0,0 +1,15 @@
+var x = 30;
+var y = 40;
+var z = 50;
+if (x == y){
+    document.write("x is equal to y")
+}
+else if (x>y){
+    document.write("x is greater than y")
+}
+else if (x<z){
+    document.write("x is leass than y")
+}
+else{
+    document.write ("all the given conditions are wrong")
+}
