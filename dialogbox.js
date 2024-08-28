2b2a646da2aa31c21ad462b1d6427099533dcdcf
prasannadevapujyula <sailakshmiprasanna0707@gmail.com>
2024-08-28 21:12:16 +0530

    commit

---
+++ dialogbox.js
@@ -0,0 +1,8 @@
+var woman = confirm("are you a student")
+if (woman){
+    let name = prompt ("please enter your highest qualification")
+
+} else{
+    let name= prompt("please select your designation")
+    
+}
